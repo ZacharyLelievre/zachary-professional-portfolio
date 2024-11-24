0bc9e1fa0ac92f01b7091b51b89ec1b0166884ef
ZacharyLelievre <lelievrezachary@gmail.com>
2024-11-24 16:53:15 -0500

    Initial commit with project files

---
+++ detailing-be/src/main/java/com/example/highenddetailing/HighEndDetailingApplication.java
@@ -0,0 +1,13 @@
+package com.example.highenddetailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighEndDetailingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighEndDetailingApplication.class, args);
+	}
+
+}
---
+++ detailing-be/src/test/java/com/example/highenddetailing/HighEndDetailingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.highenddetailing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighEndDetailingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
